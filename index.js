f5fa9c6a2a47224c5c6db1afc9ca3c680087f9ed
Ben Leung <ben.leung@dowjones.com>
2015-04-28 12:33:13 +0800

    minor fix, source for scatter chart

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ var async   = require('async');
 
 var URLS = ['http://www.aastocks.com/tc/ipo/ListedIPO.aspx',       
     'http://www.aastocks.com/tc/ipo/ListedIPO.aspx?iid=ALL&orderby=DA&value=DESC&index=2',
-    'http://www.aastocks.com/tc/ipo/ListedIPO.aspx?iid=ALL&orderby=DA&value=DESC&index=3']
+    'http://www.aastocks.com/tc/ipo/ListedIPO.aspx?iid=ALL&orderby=DA&value=DESC&index=3',
+    'http://www.aastocks.com/tc/ipo/ListedIPO.aspx?iid=ALL&orderby=DA&value=DESC&index=4',
+    'http://www.aastocks.com/tc/ipo/ListedIPO.aspx?iid=ALL&orderby=DA&value=DESC&index=5',
+    'http://www.aastocks.com/tc/ipo/ListedIPO.aspx?iid=ALL&orderby=DA&value=DESC&index=6',
+    'http://www.aastocks.com/tc/ipo/ListedIPO.aspx?iid=ALL&orderby=DA&value=DESC&index=7',
+    'http://www.aastocks.com/tc/ipo/ListedIPO.aspx?iid=ALL&orderby=DA&value=DESC&index=8',
+    'http://www.aastocks.com/tc/ipo/ListedIPO.aspx?iid=ALL&orderby=DA&value=DESC&index=9'];
 
-app.get('/scrape', function(req, res){
+var MAX_OVERSCRIBED = 500.0;  // high overscription cases are omitted for illustration purpose on scatter chart
+
+app.get('/stocks', function(req, res){
     var entries = [];
     
     async.map(URLS, function(url, cb) {
@@ -23,7 +31,7 @@ app.get('/scrape', function(req, res){
                   firstdayEarning = parseFloat($(this).children(".rgt").eq(5).text().trim())/100;
                   oversubscribed = $(this).children(".rgt").eq(2).text().trim();
                   ipoPrice = $(this).children(".rgt").eq(1).text().trim();
-                  if(/N\/A/.test(ipoPrice))   return; // not public offer
+                  if(/N\/A/.test(ipoPrice) || /N\/A/.test(oversubscribed) )   return; // not public offer
                   else{
                     oversubscribed = parseFloat(oversubscribed) || null;
                     var entry = {
@@ -39,12 +47,26 @@ app.get('/scrape', function(req, res){
       });
     },
     function(err, results){
-        var mergedResults = [];
+        var mergedResults = [], retObj;
         mergedResults = mergedResults.concat.apply(mergedResults, results.reverse());
-        res.send(mergedResults);
+        if(req.query.type==='scatter'){
+          retObj = [];
+          mergedResults.forEach(function(i){
+            if(i.oversubscribed < MAX_OVERSCRIBED){
+              retObj.push({x:i.oversubscribed || 0, y:i.firstdayEarning, name:i.company});  // oversubscribed==0 means undersubscribed
+            }
+          });
+        }
+        if(req.query.type==='bar'){
+          retObj = [];
+          mergedResults.forEach(function(i){
+            // TODO: bar chart
+          });
+        }
+        res.send(retObj || mergedResults);
     });
 })
 
 app.listen('8081')
-console.log('Magic happens on port 8081');
+console.log('Listening to port 8081');
 exports = module.exports = app;
